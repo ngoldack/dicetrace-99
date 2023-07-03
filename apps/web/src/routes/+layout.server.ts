35b13408afa103bbf9fca0b643816053a95e11cb
Nicolas Goldack <nicolas-goldack@live.de>
2023-07-03 14:12:21 +0200

    refactor: cleanup

--- apps/web/src/routes/+layout.server.ts
+++ apps/web/src/routes/+layout.server.ts
@@ -1,11 +1,7 @@
 import type { LayoutServerLoad } from './$types';
 
-import type { Game } from 'types/game';
-
 export const load: LayoutServerLoad = async (event) => {
 	const session = await event.locals.getSession();
-
-	const g: Game;
 
 	return {
 		session
