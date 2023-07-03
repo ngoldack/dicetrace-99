ccf1beba05b1408fad014491a0f5ebe199063dd5
Nicolas Goldack <nicolas-goldack@live.de>
2023-07-03 10:54:37 +0200

    feat(api/search): added rust function

--- api/onboarding/handler.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async (req: VercelRequest, res: VercelResponse) => {
-
-    // do something with req and res
-    
-    return res.status(200).send('ok');
-}
