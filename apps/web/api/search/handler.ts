2b53a769ace92e05c8977139c8732fee24841ec8
Nicolas Goldack <nicolas-goldack@live.de>
2023-07-03 12:07:36 +0200

    fix: handler

--- apps/web/api/search/handler.ts
+++ apps/web/api/search/handler.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(request: VercelRequest, response: VercelResponse) {
+export default function (request: VercelRequest, response: VercelResponse) {
 	const { name = 'World' } = request.query;
-	return response.send(`Hello ${name}!`);
+	response.send(`Hello ${name}!`);
 }
