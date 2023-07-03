059bf6a154a4a8ee19da09ef640080ccfbb221f8
Nicolas Goldack <nicolas-goldack@live.de>
2023-07-03 14:00:05 +0200

    wip

--- apps/web/src/routes/+layout.server.ts
+++ apps/web/src/routes/+layout.server.ts
@@ -1,7 +1,11 @@
 import type { LayoutServerLoad } from './$types';
 
+import type { Game } from 'types/game';
+
 export const load: LayoutServerLoad = async (event) => {
 	const session = await event.locals.getSession();
+
+	const g: Game;
 
 	return {
 		session
