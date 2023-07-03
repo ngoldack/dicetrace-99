365341a90badf96c548a598aa9959b8ed7c08516
Nicolas Goldack <nicolas-goldack@live.de>
2023-07-03 17:50:59 +0200

    feat: added version checking

--- apps/web/src/routes/app/+layout.svelte
+++ apps/web/src/routes/app/+layout.svelte
@@ -10,8 +10,11 @@
 		popup
 	} from '@skeletonlabs/skeleton';
 
+	import { updated } from '$app/stores';
 	import { page } from '$app/stores';
 
+	import RefreshIcon from '~icons/mdi/refresh';
+	import WarningIcon from '~icons/mdi/alert-circle-outline';
 	import DashboardIcon from '~icons/mdi/view-dashboard';
 	import SettingsIcon from '~icons/mdi/cog';
 	import CollectionIcon from '~icons/mdi/cube-outline';
@@ -32,83 +35,115 @@
 	export let data;
 </script>
 
-<nav>
-
+<nav class="h-full" data-sveltekit-reload={$updated ? '' : 'off'}>
 	<AppShell>
 		<svelte:fragment slot="header">
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<h1 class="h1">
-					<span
-						class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+			<AppBar>
+				<svelte:fragment slot="lead">
+					<h1 class="h1">
+						<span
+							class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+						>
+							dicetrace
+						</span>
+					</h1>
+				</svelte:fragment>
+
+				<div class="w-[50%]">
+					<div class="input-group input-group-divider grid-cols-[auto_1fr_auto] outline-none w-max">
+						<input type="search" class="outline-none p-2 w-full" placeholder="Search..." />
+						<button class="variant-filled-secondary w-full">
+							<SearchIcon />
+						</button>
+					</div>
+				</div>
+
+				<svelte:fragment slot="trail">
+					<div
+						class="flex flex-row gap-4 items-center justify-center h-fit"
+						use:popup={popupAccount}
 					>
-						dicetrace
-					</span>
-				</h1>
-			</svelte:fragment>
-
-			<div class="w-[50%]">
-				<div class="input-group input-group-divider grid-cols-[auto_1fr_auto] outline-none w-max">
-					<input type="search" class="outline-none p-2 w-full" placeholder="Search..." />
-					<button class="variant-filled-secondary w-full">
-						<SearchIcon />
-					</button>
-				</div>
-			</div>
-
-			<svelte:fragment slot="trail">
-				<div class="flex flex-row gap-4 items-center justify-center h-fit" use:popup={popupAccount}>
-					<Avatar
-						initials={data.session?.user?.name
-							?.split(' ')
-							.map((n) => n[0])
-							.join('')}
-						url={data.session?.user?.image}
-						background="bg-primary-500"
-						rounded="rounded"
-						width="w-12"
-					/>
-				</div>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<svelte:fragment slot="sidebarLeft">
-		<AppRail>
-			<svelte:fragment slot="lead">
-				<AppRailAnchor href="/app" selected={$page.url.pathname === '/app'}>
-					<svelte:fragment slot="lead">
-						<DashboardIcon class="text-xl" />
-					</svelte:fragment>
-					<span>Dashboard</span>
-				</AppRailAnchor>
-			</svelte:fragment>
-			<!-- --- -->
-			<AppRailAnchor href="/app/collection" selected={$page.url.pathname === '/app/collection'}>
-				<svelte:fragment slot="lead">
-					<CollectionIcon class="text-xl" />
+						<Avatar
+							initials={data.session?.user?.name
+								?.split(' ')
+								.map((n) => n[0])
+								.join('')}
+							url={data.session?.user?.image}
+							background="bg-primary-500"
+							rounded="rounded"
+							width="w-12"
+						/>
+					</div>
 				</svelte:fragment>
-				<span>Collection</span>
-			</AppRailAnchor>
+			</AppBar>
+		</svelte:fragment>
 
-			<AppRailAnchor href="/app/history" selected={$page.url.pathname === '/app/history'}>
+		<svelte:fragment slot="pageHeader">
+			<!-- https://github.com/getsentry/sentry-javascript/discussions/5838#discussioncomment-6310009 -->
+			{#if $updated}
+				<div class="p-2">
+					<aside class="alert variant-filled-warning">
+						<!-- Icon -->
+						<div>
+							<WarningIcon class="text-3xl" />
+						</div>
+						<!-- Message -->
+
+						<div class="alert-message">
+							<h3 class="h3">App updated!</h3>
+							<p>This app has been updated, please refresh.</p>
+						</div>
+						<!-- Actions -->
+						<div class="alert-actions">
+							<button class="btn variant-outline-primary" on:click={() => location.reload()}>
+								<div>
+									<RefreshIcon class="text-xl" />
+								</div>
+								Refresh
+							</button>
+						</div>
+					</aside>
+				</div>
+			{/if}
+		</svelte:fragment>
+
+		<svelte:fragment slot="sidebarLeft">
+			<AppRail>
 				<svelte:fragment slot="lead">
-					<HistoryIcon class="text-xl" />
+					<AppRailAnchor href="/app" selected={$page.url.pathname === '/app'}>
+						<svelte:fragment slot="lead">
+							<DashboardIcon class="text-xl" />
+						</svelte:fragment>
+						<span>Dashboard</span>
+					</AppRailAnchor>
 				</svelte:fragment>
-				<span>History</span>
-			</AppRailAnchor>
-			<!-- --- -->
-			<svelte:fragment slot="trail">
-				<AppRailAnchor href="/app/settings" selected={$page.url.pathname === '/app/settings'}>
+				<!-- --- -->
+				<AppRailAnchor href="/app/collection" selected={$page.url.pathname === '/app/collection'}>
 					<svelte:fragment slot="lead">
-						<SettingsIcon class="text-xl" />
+						<CollectionIcon class="text-xl" />
 					</svelte:fragment>
-					<span>Settings</span>
+					<span>Collection</span>
 				</AppRailAnchor>
-			</svelte:fragment>
-		</AppRail>
-	</svelte:fragment>
-	<slot />
-</AppShell>
+
+				<AppRailAnchor href="/app/history" selected={$page.url.pathname === '/app/history'}>
+					<svelte:fragment slot="lead">
+						<HistoryIcon class="text-xl" />
+					</svelte:fragment>
+					<span>History</span>
+				</AppRailAnchor>
+				<!-- --- -->
+				<svelte:fragment slot="trail">
+					<AppRailAnchor href="/app/settings" selected={$page.url.pathname === '/app/settings'}>
+						<svelte:fragment slot="lead">
+							<SettingsIcon class="text-xl" />
+						</svelte:fragment>
+						<span>Settings</span>
+					</AppRailAnchor>
+				</svelte:fragment>
+			</AppRail>
+		</svelte:fragment>
+		<slot />
+	</AppShell>
 </nav>
 
 <div class="card p-4 w-72 shadow-xl" data-popup="popupAccount">
