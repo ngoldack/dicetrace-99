5fabbd8b758e19c1327900d543d04264e9450c6b
Nicolas Goldack <nicolas-goldack@live.de>
2023-07-03 11:29:01 +0200

    feat(web): added service worker

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vitest/config';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		SvelteKitPWA(),
 		Icons({
 			compiler: 'svelte'
 		})
