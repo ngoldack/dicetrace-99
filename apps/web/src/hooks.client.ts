3aefdbb403db5a4aa07461febd2e1c1d90f89c9d
Goldack, Nicolas <Nicolas.Goldack@arag.de>
2023-07-09 18:01:44 +0200

    style: formatted with prettier

--- apps/web/src/hooks.client.ts
+++ apps/web/src/hooks.client.ts
@@ -1,20 +1,20 @@
-import { handleErrorWithSentry, Replay } from "@sentry/sveltekit";
+import { handleErrorWithSentry, Replay } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
-  dsn: 'https://098e1022501148f4a3e5466b7b8ea4f6@o4505465647267840.ingest.sentry.io/4505465648578560',
-  tracesSampleRate: 1.0,
+	dsn: 'https://098e1022501148f4a3e5466b7b8ea4f6@o4505465647267840.ingest.sentry.io/4505465648578560',
+	tracesSampleRate: 1.0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+	// This sets the sample rate to be 10%. You may want this to be 100% while
+	// in development and sample at a lower rate in production
+	replaysSessionSampleRate: 0.1,
 
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-  
-  // If you don't want to use Session Replay, just remove the line below:
-  integrations: [new Replay()],
+	// If the entire session is not sampled, use the below sample rate to sample
+	// sessions when an error occurs.
+	replaysOnErrorSampleRate: 1.0,
+
+	// If you don't want to use Session Replay, just remove the line below:
+	integrations: [new Replay()]
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,16 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-	plugins: [sentrySvelteKit(), sveltekit(), Icons({
-        compiler: 'svelte'
-    })],
+	plugins: [
+		sentrySvelteKit(),
+		sveltekit(),
+		Icons({
+			compiler: 'svelte'
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
